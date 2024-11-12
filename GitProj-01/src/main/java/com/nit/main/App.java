b19ca864bd83773baca2716a113c92ef4db63f07
subba <subba@DESKTOP-A1B34KB.domain.name>
2024-11-12 21:37:38 +0530

    TL change 44

--- GitProj-01/src/main/java/com/nit/main/App.java
+++ GitProj-01/src/main/java/com/nit/main/App.java
@@ -8,7 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		App app = new App();
 		int sum1 = app.sum(100, 200);
-		 System.out.println("sum of22 value:"+sum1);
+		 System.out.println("sum of44 value:"+sum1);
 		//System.out.println("sum result value:" + app.sum(100, 200));
 		// System.out.println("sum of value:"+sum1);
 	}
