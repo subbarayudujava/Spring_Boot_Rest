53dae5ee0d23c26b5feff6ae9499d57f9208f9fc
subba <subba@DESKTOP-A1B34KB.domain.name>
2024-11-13 10:17:31 +0530

    TL....Inhancing the code

--- GitProj-01/src/main/java/com/nit/upi/UpiPaments.java
+++ GitProj-01/src/main/java/com/nit/upi/UpiPaments.java
@@ -1,7 +1,12 @@
 package com.nit.upi;
 
+import java.util.Random;
+
 public class UpiPaments {
-   public String upiPaments(String upiAmount,float amount) {
+   public String upiPaments(String upiAmount,Double amount) {
 	   return "Amount  done::"+amount+"paid";
+   }
+   public double checkBalance() {
+	   return new Random().nextDouble(120000.00);
    }
 }
